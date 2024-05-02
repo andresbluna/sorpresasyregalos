0035c3448eb2e3bde5772acc32a460cd24f3806c
andresbluna <andres.benjamin.luna@gmail.com>
2024-05-01 20:34:19 -0400

    Create method user Service

--- backend/api/users/src/main/java/com/register/users/service/UserService.java
+++ backend/api/users/src/main/java/com/register/users/service/UserService.java
@@ -1,12 +1,13 @@
 package com.register.users.service;
-
 import com.register.users.model.User;
 import com.register.users.repository.UserRepository;
+import jakarta.validation.constraints.Null;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
     private final UserRepository userRepository;
 
     @Autowired
@@ -14,32 +15,23 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public User createUser(User user) {
-        // Aquí puedes agregar alguna lógica de validación de datos antes de guardar el usuario en la base de datos
+    public User createUser (User user) {
 
-        // llama a save() del repositorio para almacenar el usuario en la base de datos
-        return userRepository.save(user);
-    }
-    public User updateUser(Long id, User newUser) {
-        return userRepository.findById(id)
-                .map(user -> {
-                    // Aquí estableces todos los campos del usuario existente con los del nuevo usuario
-                    user.setName(newUser.getName());  // Suponiendo que tenga un campo llamado "name"
-                    // Agrega aquí todos los demás campos del usuario
-                    return userRepository.save(user);
-                })
-                .orElseThrow(() -> new NotFoundException("User not found with id " + id));
-    }
-
+        if (user != null){
+            userRepository.save(user);
+        }
+        return createUser(user);
 
     }
-public User getUser(Long id) {
-    if (userRepository.existsById(id)) {
-        return userRepository.findById(id).orElse(null);
-    } else {
-        throw new NotFoundException("User not found with id " + id);
+
+
+    public User updateUser (User user) {
+
+        if (user != null) {
+
+
+
+        }
+        return updateUser(user);
     }
-}
-
-
 }
